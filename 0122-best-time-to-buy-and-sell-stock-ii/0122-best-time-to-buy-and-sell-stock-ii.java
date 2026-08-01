e2848fd7490e716fa235edbbc72f179de2006cae
asmit0907 <royasmit80@gmail.com>
2026-08-01 06:32:07 +0530

    Time: 0 ms (100%), Space: 46.5 MB (50.21%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,16 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length < 2) {
+        if(prices==null ||  prices.length<2)
             return 0;
-        }
-        
-        int maxProfit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                maxProfit += prices[i] - prices[i - 1];
+
+        int mp=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]>prices[i-1]){
+                mp+=prices[i]-prices[i-1];
             }
         }
-        
-        return maxProfit;
+        return mp;
     }
 }
