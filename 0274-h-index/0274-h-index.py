87310e8773282e6580eb8cb6da469ae91ea2c783
asmit0907 <royasmit80@gmail.com>
2026-08-31 08:42:51 +0530

    Time: 546 ms (5.01%), Space: 19.4 MB (65.42%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -4,17 +4,17 @@ class Solution:
         max_h = 0
         
         # Test every possible H-index candidate from 0 to n
-        for potential_h in range(n + 1):
+        for i in range(n + 1):
             count = 0
             
-            # Count how many papers have AT LEAST potential_h citations
+            # Count how many papers have AT LEAST i citations
             for j in range(n):
-                if citations[j] >= potential_h:
+                if citations[j] >= i:
                     count += 1
             
-            # If we found at least potential_h papers, it's a valid H-index
-            if count >= potential_h:
-                max_h = max(max_h, potential_h)
+            # If we found at least i papers, it's a valid H-index
+            if count >= i:
+                max_h = max(max_h, i)
                 
         return max_h
         
